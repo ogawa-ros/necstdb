f82dc41d6b331d2e944e7ca1950ae1c4e7dcba53
Shota Ueda <ueda@s7u27m.local>
2019-06-20 21:38:03 +0900

    refactoring-necstdb

--- necstdb/necstdb.py
+++ necstdb/necstdb.py
@@ -9,102 +9,94 @@ class necstdb(object):
 
     def __init__(self):
         self.con = None
+        self.cur = None
+        self.db_path = ''
         pass
 
     def __del__(self):
         self.con.close()
         return
 
-    def open(self, dbpath):
-        self.con = sqlite3.connect(dbpath, check_same_thread=False)
+    def _check_status(self):
+        pass
+
+    def open(self, db_path):
+        self._check_status()
+        self.db_path = db_path
+        self.con = sqlite3.connect(db_path, check_same_thread=False)
+        self.con.execute("CREATE table if not exists 'necst' ('topic', 'time', 'msgs')")
+        self.cur = self.con.cursor()
         return
         
-    def mk_cur(self, num):
-        self.cur = []
-        for i in range(num):
-            self.cur.append(self.con.cursor())
-        return
-
     def commit_data(self):
+        self._check_status()
         self.con.commit()
         return
 
     def close(self):
+        self._check_status()
         self.con.close()
+        self.db_path = ''
+        self.con = None
+        self.cur = None
         return
 
-    def make_table(self, table_name, param):
-        self.con.execute("CREATE table if not exists {} {}".format(table_name, param))
-        return
-
-    def write(self, table_name, param, values, cur_num=0, auto_commit = False):
-        if len(values) == 1:
-            quest = "?"
-        else:
-            tmp = ""
-            quest = ",".join([tmp + "?" for i in range(len(values))])
-
+    def write(self, values, auto_commit = False):
+        table_name = 'necst'
+        quest = "?, ?, ?"
+        param = ('topic', 'time', 'msgs')
         val = []
-        for i in range(len(values)):
-            if type(values[i]) == list:
-                val.append(pickle.dumps(values[i]))
-            elif type(values[i]) == tuple:
-                val.append(pickle.dumps(list(values[i])))
-            else:
-                val.append(values[i])
+        val.append(values['topic'])
+        val.append(values['time'])
+        val.append(pickle.dumps(values['msgs']))
         values = tuple(val)
-
-        if auto_commit:
-            with self.con:
-                self.cur[cur_num].execute("INSERT into {0} {1} values ({2})".format(table_name, param, quest), values)
-        else:
-            self.cur[cur_num].execute("INSERT into {0} {1} values ({2})".format(table_name, param, quest), values)
+        self.cur.execute("INSERT into {0} {1} values ({2})".format(table_name, param, quest), values)
         return
         
-    def writemany(self, table_name, param, values, cur_num=0, auto_commit = False):
-        if len(values[0]) == 1:
-            quest = "?"
-        else:
-            tmp = ""
-            quest = ",".join([tmp + "?" for i in range(len(values[0]))])
+#    def writemany(self, table_name, param, values, auto_commit = False):
+#        if len(values[0]) == 1:
+#            quest = "?"
+#        else:
+#            tmp = ""
+#            quest = ",".join([tmp + "?" for i in range(len(values[0]))])
+#
+#        if auto_commit:
+#            with self.con:
+#                self.cur.executemany("INSERT into {0} {1} values ({2})".format(table_name, param, quest), values)
+#        else:
+#            self.cur.executemany("INSERT into {0} {1} values ({2})".format(table_name, param, quest), values)
+#        return
 
-        if auto_commit:
-            with self.con:
-                self.cur[cur_num].executemany("INSERT into {0} {1} values ({2})".format(table_name, param, quest), values)
-        else:
-            self.cur[cur_num].executemany("INSERT into {0} {1} values ({2})".format(table_name, param, quest), values)
-        return
-
-    def read(self, table_name, param="*", cur_num=0):
-        row = self.cur[cur_num].execute("SELECT {0} from {1}".format(param, table_name)).fetchall()
+    def read(self, param="*"):
+        table_name = 'necst'
+        row = self.cur.execute("SELECT {0} from {1}".format(param, table_name)).fetchall()
         if not row == []:
-            data = [
-                [row[i][j] for i in range(len(row))] 
-                    for j in range(len(row[0]))
-                    ]
+            data = [[row[i][j] for i in range(len(row))] 
+                    for j in range(len(row[0]))]
         else : data = []
-        
         dat = []
         for i in range(len(data)):
             if type(data[i][0]) == bytes:
                 dat.append([pickle.loads(data[i][j]) for j in range(len(data[i]))])
             else:
                 dat.append(data[i])
-
         return dat
 
-    def read_as_pandas(self, table_name):
+    def read_as_pandas(self):
+        table_name = 'necst'
         df = pandas.read_sql("SELECT * from {}".format(table_name), self.con)
+        for i in range(len(df['msgs'])):
+            df.loc[i, 'msgs'] = [pickle.loads(df['msgs'][i])]
         return df
 
-    def read_pandas_all(self):
-        table_name = self.get_table_name()
-        datas = [self.read_as_pandas(name) for name in table_name]
-        if datas ==[]:
-            df_all = []
-        else:
-            df_all = pandas.concat(datas, axis=1)
-        return df_all
+#    def read_pandas_all(self):
+#        table_name = self.get_table_name()
+#        datas = [self.read_as_pandas(name) for name in table_name]
+#        if datas ==[]:
+#            df_all = []
+#        else:
+#            df_all = pandas.concat(datas, axis=1)
+#        return df_all
 
     def check_table(self):
         row = self.con.execute("SELECT * from sqlite_master")
@@ -116,31 +108,23 @@ class necstdb(object):
         name_list = sorted([name[i][0] for i in range(len(name))])
         return name_list
 
-    def insert(self,dbpath,topic_name,data):
+    def insert(self, dic):
         if self.con is None:
-            if os.path.exits(self.dbpath[:self.dbpath.rfind('/')]):pass
-            else: os.makedirs(self.dbpath[:self.dbpath.rfind('/')])
-             
-            self.open(dbpath)
+            self.db_path = dic['path']
+            if os.path.exists(self.db_path[:self.db_path.rfind('/')]): pass
+            else: os.makedirs(self.db_path[:self.db_path.rfind('/')])
+            self.open(self.db_path)
         else:
-            if self.last_dbpath != dbpath:  
+            if dic['path'] != self.db_path:  
                 self.finalize()
-
-                if os.path.exits(self.dbpath[:self.dbpath.rfind('/')]):pass  
-                else: os.makedirs(self.dbpath[:self.dbpath.rfind('/')]) 
-        
-                self.open(dbpath)
-            else:pass
-
-        self.make_table('raw_data',"(topic_name, data)")
-        # pythonでifにするとSQLでif not existsするの、どっちが速い？
-
-        self.write('raw_data', "", (topic_name,data), cur_num = 1, auto_commit = False)
-        self.last_path = path
+                if os.path.exists(self.db_path[:self.db_path.rfind('/')]): pass
+                else: os.makedirs(self.db_path[:self.db_path.rfind('/')])
+                self.open(dic['path'])
+            else: pass
+        self.write(dic['data'])
         return
 
     def finalize(self):
         self.commit_data()
-        self.con.close()
-        self.con = None
+        self.close()
         return
