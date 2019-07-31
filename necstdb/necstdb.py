e31771b9af7c9ab6bfdfaf54ebdee86243ac4794
s7u27g <s7u27astr0@gmail.com>
2019-07-31 11:58:47 +0900

    version-0.2.0

--- necstdb/necstdb.py
+++ necstdb/necstdb.py
@@ -1,98 +1,131 @@
 #!/usr/bin/env python3
 
-import os
-import sqlite3
-import pickle
-import pandas
+import json
+import pathlib
+import struct
 
 class necstdb(object):
+    path = ''
 
-    def __init__(self):
-        self.con = None
-        self.cur = None
-        self.db_path = ''
+    def __init__(self, path):
+        self.opendb(path)
         pass
 
-    def open(self, db_path):
-        self.db_path = db_path
-        self.con = sqlite3.connect(db_path, check_same_thread=False)
-        self.con.execute("CREATE table if not exists 'necst' ('topic', 'time', 'msgs')")
-        self.cur = self.con.cursor()
+    def opendb(self, path):
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+            pass
+
+        self.path = path
+        path.mkdir(exist_ok=True)
         return
-        
-    def commit_data(self):
-        self.con.commit()
+
+    def list_tables(self):
+        return [t.stem for t in self.path.glob('*.data')]
+
+    def create_table(self, name, config):
+        list = list_tables()
+        if name in list:
+            pass
+        else:
+            data = self.path / (name + '.data')
+            data.touch()
+            header = self.path / (name + '.header')
+            with header.open(mode='w') as f:
+                json.dump(config,f)
+                pass
+            return
+
+    def open_table(self, name, mode='rb'):
+        table_ = table(self.path, name, mode)
+        return table_
+
+
+class table(object):
+    dbpath = ''
+    fdata = None
+    header = {}
+
+    def __init__(self, dbpath, name, mode):
+        self.dbpath = dbpath
+        self.open(name, mode)
+        pass
+
+    def open(self, table, mode):
+        data = self.dbpath / (table + '.data')
+        header = self.dbpath / (table + '.header')
+
+        if not(data.exists() and header.exists()):
+            raise(Exception("table '{name}' does not exist".format(**locals())))
+
+        self.fdata = open(data, mode)
+        fheader = open(header, 'r')
+        self.header = json.load(fheader)
+        fheader.close()
+        self.record_size = sum([h['size'] for h in self.header['data']])
         return
 
     def close(self):
-        self.con.close()
-        self.db_path = ''
-        self.con = None
-        self.cur = None
+        fdata.close()
         return
 
-    def write(self, values, auto_commit = False):
-        table_name = 'necst'
-        quest = "?, ?, ?"
-        param = ('topic', 'time', 'msgs')
-        val = []
-        val.append(values['topic'])
-        val.append(values['time'])
-        val.append(pickle.dumps(values['msgs']))
-        values = tuple(val)
-        self.cur.execute("INSERT into {0} {1} values ({2})".format(table_name, param, quest), values)
+    def append(self, *data):
+        format = ''.join([h['format'] for h in self.header['data']])
+        self.fdata.write(struct.pack(format, *data))
         return
-    
-    def read(self, param="*"):
-        table_name = 'necst'
-        self.cur.execute("SELECT {0} from {1}".format(param, table_name))
-        ret = []
-        for row in self.cur.fetchall():
-            dic = dict(zip([d[0] for d in self.cur.description], row))
-            dic['msgs'] = pickle.loads(dic['msgs'])
-            ret.append(dic)
-        return ret
-    
-    def read_as_pandas(self):
-        table_name = 'necst'
-        ret = pandas.read_sql("SELECT * from {}".format(table_name), self.con)
-        for i in ret.index:
-            ret.at[i, 'msgs'] = [pickle.loads(ret['msgs'][i])]
-        return ret
 
-    def check_table(self):
-        self.cur.execute("SELECT * from sqlite_master")
-        ret = self.cur.fetchall()
-        return ret
+    def read(self, num=-1, start=0, cols=[], astype=None):
+        self.fdata.seek(start * self.record_size)
 
-    def get_table_name(self):
-        self.cur.execute("SELECT name from sqlite_master where type='table'")
-        ret = sorted([i[0] for i in self.cur.fetchall()])
-        return ret
-
-
-###=== for necst-core/logger ===###
-
-    def insert(self, dic):
-        if self.con is None:
-            self.db_path = dic['path']
-            if os.path.exists(self.db_path[:self.db_path.rfind('/')]): pass
-            else: os.makedirs(self.db_path[:self.db_path.rfind('/')])
-            self.open(self.db_path)
+        if cols == []:
+            d = self._read_all_cols(num)
         else:
-            if dic['path'] != self.db_path:  
-                self.finalize()
-                if os.path.exists(self.db_path[:self.db_path.rfind('/')]): pass
-                else: os.makedirs(self.db_path[:self.db_path.rfind('/')])
-                self.open(dic['path'])
-            else: pass
-        self.write(dic['data'])
-        return
+            d = self._read_specified_cols(num, cols)
+            pass
 
-    def finalize(self):
-        if self.con is None: pass
+        return self._astype(d, cols, astype)
+
+    def _read_all_cols(self, num):
+        if num == -1:
+            size = num
         else:
-            self.commit_data()
-            self.close()
+            size = num * self.record_size
+            pass
+        return self.fdata.read(size)
+
+    def _read_specified_cols(self, num, cols):
+        sizes = []
+        seeks = []
+        i = 0
+        draw = b''
+        while i < num:
+            for size, seek in zip(sizes, seeks):
+                draw += self.fdata.read(size)
+                self.fdata.seek(seek, 1)
+                continue
+            i += 1
+            continue
+        return draw
+
+    def _astype(self, data, cols, astype):
+        if cols == []:
+            cols = self.header['data']
+        else:
+            cols = [c for c in self.header['data'] if c['key'] in cols]
+            pass
+
+        if astype in [None, 'tuple']:
+            return self._astype_tuple(data, cols)
+        if astype in ['dict']:
+            return self._astype_dict(data, cols)
+        elif astype in ['pandas']:
+            return self._astype_pandas(data, cold)
         return
 
+    def _astype_tuple(self, data, cols):
+        format = ''.join([c['format'] for c in cols])
+        return tuple(struct.iter_unpack(format, data))
+
+
+def opendb(path):
+    return necstdb(path)
